62864f4db9e915b6998b714baa6546e76e15c4e1
Diego Herrera <dherrera@binarybag.com>
2019-06-06 11:23:05 -0400

    Updates

---
+++ precache-manifest.55f15887d106c1f5fa0eb6fae1570583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab485dbf28f3ac09d90532d888e8ffcc",
+    "url": "/proyecto_2/index.html"
+  },
+  {
+    "revision": "d8122c5a6e6c03bd07f5",
+    "url": "/proyecto_2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "42048a2c640a16597892",
+    "url": "/proyecto_2/static/js/2.6a9d4381.chunk.js"
+  },
+  {
+    "revision": "d8122c5a6e6c03bd07f5",
+    "url": "/proyecto_2/static/js/main.38b8bcae.chunk.js"
+  },
+  {
+    "revision": "34c749336441802aff4d",
+    "url": "/proyecto_2/static/js/runtime~main.6f9804e2.js"
+  }
+]);
